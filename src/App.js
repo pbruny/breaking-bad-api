8885caeac609c83d1b4da78debcc69e9592159dc
Paulo Lima <paulobruny@gmail.com>
2020-07-12 21:46:45 -0300

    add style and update main app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Header from './components/Header'
+import Character from './components/Character'
+import Search from './components/Search'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState((true))
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setItems(response)
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [query])
+
   return (
-    <div className="App">
-      <h1>Hallo</h1>
+    <div className="container">
+      <Header />
+      <Search getQuery={(query) => setQuery(query)} />
+      <Character isLoading={isLoading} items={items.data} />
     </div>
   );
 }
